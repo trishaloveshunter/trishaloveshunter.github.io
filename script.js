3070a82b82a5d428395797c958fba9459919f4b4
trishaloveshunter <trishaloveshunter@gmail.com>
2024-09-05 22:53:18 -0700

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const gif = document.querySelector(".gif");
 // Change text and gif when the Yes button is clicked
 yesBtn.addEventListener("click", () => {
   question.innerHTML = "Yayy I love youu 🥰 ";
-  gif.src = "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExZGI1cW5wMWhpaDF5b3pjdTF0OHZrcHJvaGkzOHJteDhmd245OGRnZyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Vuw9m5wXviFIQ/giphy.gif";
+  gif.src = "https://tenor.com/rR97ajL2U6c.gif";
 });
 
 // Make the No button move randomly on hover
